3daa9a00ff37ad2f582e60517854a09d7cdad849
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2020-08-31 14:52:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/labs3eco/NameActivity.java
@@ -0,0 +1,19 @@
+package com.example.labs3eco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class NameActivity extends AppCompatActivity {
+
+
+    private Button bConfgMain2;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
