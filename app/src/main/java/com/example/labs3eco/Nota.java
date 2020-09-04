23710eeaebd92e79e7961c65b9e13811bc0d5975
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2020-09-03 19:08:17 -0500

    ahi vamos

---
+++ app/src/main/java/com/example/labs3eco/Nota.java
@@ -0,0 +1,17 @@
+package com.example.labs3eco;
+
+public class Nota {
+
+    private int nota;
+    private int porcen;
+
+    public Nota(int nota){
+
+        this.nota=nota;
+
+    }
+
+    public void calcular(){
+
+    }
+}
